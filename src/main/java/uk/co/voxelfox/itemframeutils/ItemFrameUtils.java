280cb86240434259ab9a3908c3e648d3451d9789
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-05 02:17:25 +0000

    Add limit, sorting, and combined the two commands into one

--- src/main/java/uk/co/voxelfox/itemframeutils/ItemFrameUtils.java
+++ src/main/java/uk/co/voxelfox/itemframeutils/ItemFrameUtils.java
@@ -11,6 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import de.tr7zw.nbtapi.NBTEntity;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public final class ItemFrameUtils extends JavaPlugin {
@@ -27,10 +28,13 @@ public final class ItemFrameUtils extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("setframeinvisible")) {
-            int distance;
+        if(command.getName().equalsIgnoreCase("setframevisible")) {
+            if(!(args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("false"))) {
+                return false;
+            }
+            int distance, limit;
             try {
-                distance = Integer.parseInt(args[0]);
+                distance = Integer.parseInt(args[1]);
                 assert distance <= 100;
             }
             catch(NumberFormatException e) {
@@ -40,40 +44,39 @@ public final class ItemFrameUtils extends JavaPlugin {
                 sender.sendMessage("You can only have a distance of up to 100 blocks.");
                 return true;
             }
-            setItemFrameInvisible(sender, true);
-            return true;
-        }
-        else if(command.getName().equalsIgnoreCase("setframevisible")) {
-            int distance;
             try {
-                distance = Integer.parseInt(args[0]);
-                assert distance <= 100;
+                limit = Integer.parseInt(args[2]);
+            }
+            catch(IndexOutOfBoundsException e) {
+                limit = -1;
             }
             catch(NumberFormatException e) {
                 return false;
             }
-            catch(AssertionError e) {
-                sender.sendMessage("You can only have a distance of up to 100 blocks.");
-                return true;
-            }
-            setItemFrameInvisible(sender, false);
+            setItemFrameInvisible(sender, distance, args[0].equalsIgnoreCase("false"), limit);
             return true;
         }
 
         return false;
     }
 
-    private void setItemFrameInvisible(CommandSender sender, boolean invisible) {
+    private void setItemFrameInvisible(CommandSender sender, int distance, boolean invisible, int limit) {
         Server s = sender.getServer();
         Player p = s.getPlayer(sender.getName());
-        Location l = p.getLocation();
-        List<Entity> itemFrames = new ArrayList<>(l.getNearbyEntities(l.getX(), l.getY(), l.getZ()));
+        Location l = p.getEyeLocation();
+        List<Entity> itemFrames = new ArrayList<>(l.getNearbyEntities(distance, distance, distance));
+        itemFrames.sort((o1, o2) -> o1.getLocation().distance(l) < o2.getLocation().distance(l) ? -1 : 1);
         int counter = 0;
         for(Entity e: itemFrames) {
             if(e.getType() == EntityType.ITEM_FRAME) {
                 NBTEntity entityData = new NBTEntity(e);
-                entityData.setBoolean("Invisible", invisible);
-                counter++;
+                if(entityData.getBoolean("Invisible") == !invisible) {
+                    entityData.setBoolean("Invisible", invisible);
+                    counter++;
+                    if(limit > 0 && counter >= limit) {
+                        break;
+                    }
+                }
             }
         }
         sender.sendMessage(String.format("Changed the data of %d item frames.", counter));
